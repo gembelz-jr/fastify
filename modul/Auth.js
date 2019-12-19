72db1bff1a497f04a3dbec35eb30a163e54b2976
Rizqitanto Widiantoro <tanyarizqi20@gmail.com>
2019-12-19 16:02:10 +0700

    fastify first-upload

--- modul/Auth.js
+++ modul/Auth.js
@@ -73,7 +73,37 @@ async function getSecret (secret) {
         })
     );
 }
+async function checkToken (request, reply) {
+    let token = request.body.token.toString();
+    let user_token = request.body.user_token.toString();
+    let now = moment().format('YYYY-MM-DD HH:mm:ss').toString();
+
+    let sql = `SELECT authentication.*, users.remember_token FROM authentication 
+    INNER JOIN users ON users.id = authentication.user_id WHERE authentication.id = ? AND users.remember_token = ?`;
+    let data = await new Promise((resolve) =>
+        connection.query(sql, [token, user_token], function (error, rows) {
+            if(error){
+                console.log(error);
+                return response.badRequest('',  `${error}`, reply)
+            }
+
+            if(rows.length > 0){
+                return rows[0].remember_token === user_token ? resolve(rows[0].expires_at) : resolve(false);
+            }
+            else{
+                return response.badRequest({}, "Token yang kamu masukkan salah!", reply);
+            }
+
+        })
+    );
+
+    let array = { expires_at: data};
+    let message = moment(data).format('YYYY-MM-DD HH:mm:ss').toString() > now ?
+        'Token ini milikmu dan masih aktif hingga saat ini!' : "Token kamu sudah tidak aktif!";
+
+    return array ? response.ok(array, message, reply) : response.badRequest({}, message, reply);
+}
 
 module.exports = {
-    createToken
+    createToken, checkToken
 };
--- routes/Routes.js
+++ routes/Routes.js
@@ -10,6 +10,56 @@ async function routes (fastify, options) {
     fastify.post('/api/users/register', users.register);
     fastify.post('/api/users/login', users.login);
     fastify.post('/api/users/token', auth.createToken);
+    fastify.post('/api/users/check', auth.checkToken);
+    fastify.route({
+        method: 'GET',
+        url: '/api/todo',
+        preHandler: async function (request, reply, done) {
+            await middleware.check(request, reply);
+            done()
+        },
+        handler: todo.get
+    });
+
+    fastify.route({
+        method: 'GET',
+        url: '/api/todo/:id',
+        preHandler: async function (request, reply, done) {
+            await middleware.check(request, reply);
+            done()
+        },
+        handler: todo.show
+    });
+
+    fastify.route({
+        method: 'POST',
+        url: '/api/todo',
+        preHandler: async function (request, reply, done) {
+            await middleware.check(request, reply);
+            done()
+        },
+        handler: todo.store
+    });
+
+    fastify.route({
+        method: 'PUT',
+        url: '/api/todo',
+        preHandler: async function (request, reply, done) {
+            await middleware.check(request, reply);
+            done()
+        },
+        handler: todo.update
+    });
+
+    fastify.route({
+        method: 'DELETE',
+        url: '/api/todo',
+        preHandler: async function (request, reply, done) {
+            await middleware.check(request, reply);
+            done()
+        },
+        handler: todo.destroy
+    });
 }
 
 module.exports = routes;
